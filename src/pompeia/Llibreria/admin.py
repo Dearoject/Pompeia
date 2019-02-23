32647d9b507976167ae45454dfe6b9511f1f1d17
TresLos <asistare7@gmail.com>
2019-02-23 17:20:11 +0100

    Poc a poc
    Vaig progresant
    TODO llegir commits!!

--- src/pompeia/Llibreria/admin.py
+++ src/pompeia/Llibreria/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from .models import Albarà, Proveïdor
+from .models import *
 
-@admin.register(Proveïdor)
-class ProveïdorAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(Encuadernació)
+admin.site.register(Idioma)
+admin.site.register(Tema)
+admin.site.register(Ciutat)
+admin.site.register(CoŀleccióNom)
+admin.site.register(Departament)
+admin.site.register(Editorial)
+admin.site.register(Observacions)
+admin.site.register(Autor)
+admin.site.register(Representant)
+admin.site.register(Traductor)
+admin.site.register(Adreça)
+admin.site.register(Coŀlecció)
+admin.site.register(Proveïdor)
 
 @admin.register(Albarà)
 class AlbaràAdmin(admin.ModelAdmin):
     fields = ('proveïdor', 'data', 'tipus', 'número')
+    list_display = fields
+#   list_editable = ('tipus',)
+
+admin.site.register(Catàleg)
+admin.site.register(MetaLlibre)
+admin.site.register(Llibre)
--- src/pompeia/Llibreria/models.py
+++ src/pompeia/Llibreria/models.py
@@ -38,7 +38,7 @@ class Autor(models.Model):
     Primer_nom = models.CharField(max_length = 30)
     Segon_nom = models.CharField(max_length = 30)
 
-class Persona(models.Model):
+class Representant(models.Model):
     Primer_nom = models.CharField(max_length = 30)
     Segon_nom = models.CharField(max_length = 30)
 
@@ -62,8 +62,10 @@ class Proveïdor(models.Model):
     adreça = models.ForeignKey(Adreça, on_delete = models.PROTECT)
     telèfon = models.CharField(max_length = 20)
     correu_electrònic = models.EmailField()
-    representant = models.ForeignKey(Persona, on_delete = models.PROTECT)
+    representant = models.ForeignKey(Representant, on_delete = models.PROTECT)
     descompte = models.DecimalField(max_digits = 3, decimal_places = 2)
+    def __str__(self):
+        return self.nom
 
 class Albarà(models.Model):
     proveïdor = models.ForeignKey(Proveïdor, on_delete = models.PROTECT)
@@ -71,6 +73,10 @@ class Albarà(models.Model):
     número = models.CharField(max_length = 20)
     # * client = models.PositiveIntegerField() (?)
     tipus = models.CharField(max_length = 1, choices = ALBARÀ_CHOICES)
+    class Meta:
+        verbose_name_plural = "Albarans"
+    def __str__(self):
+        return str(self.data)
 
 class Catàleg(models.Model):
     ISBN = models.CharField(max_length = 13)
@@ -86,8 +92,8 @@ class Catàleg(models.Model):
 
 class MetaLlibre(models.Model):
     catàleg = models.ForeignKey(Catàleg, on_delete = models.PROTECT)
-    tamany_alt = models.DecimalField(max_digits = 2, decimal_places = 1)
-    tamany_fons = models.DecimalField(max_digits = 2, decimal_places = 1)
+    tamany_alt = models.DecimalField(max_digits = 4, decimal_places = 1)
+    tamany_fons = models.DecimalField(max_digits = 4, decimal_places = 1)
     edició = models.CharField(max_length = 3)
     editorial = models.ForeignKey(Proveïdor, on_delete = models.PROTECT)
     coŀlecció = models.ForeignKey(Coŀlecció, on_delete = models.PROTECT,
